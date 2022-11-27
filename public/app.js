1570169c9351e4aab1e1c4ebb682c00e48915346
forstman <sami.hafid.hs@gmail.com>
2022-11-28 00:19:44 +0100

    finish the server side we just need some good ui to make the website look good

--- public/app.js
+++ public/app.js
@@ -16,33 +16,10 @@ function notFound() {
 	big_container.style.display = "none";
 }
 
-// form.addEventListener('submit', async function (){
-// 	event.preventDefault();
-// 	text = document.getElementById('data').value;
-// 	// console.log(text);
-// 	const api = await fetch('http://api.weatherstack.com/current?access_key=5bb614ec38b8edc9232ca34664b27e7a&query=' + text);
-// 	let testing = await api.json();
-// 	if (testing.success === false)
-// 	{
-// 		notFound();
-// 		return 0;
-// 	}
-// 	else
-// 		notfoundmessage.style.display = "none";
-// 	gif.innerHTML = "<img src='" + testing.current.weather_icons[0] + "' >";
-// 	weather_state.innerText = testing.current.weather_descriptions[0];
-// 	degree.textContent = testing.current.temperature + "°C";
-// 	city_name.textContent = testing.location.name + " ( " + testing.location.country + " )";
-// 	big_container.style.display = "block";
-// 	console.log(testing.success);
-// });
-
-
 form.addEventListener('submit', async function (){
 	event.preventDefault();
 	text = document.getElementById('data').value;
-	// console.log(text);
-	const api = await fetch('http://localhost:3000/' + text, {
+	const api = await fetch('http://localhost:3000/api/' + text, {
 		method: 'GET'
 	});
 	let data = await api.json();
